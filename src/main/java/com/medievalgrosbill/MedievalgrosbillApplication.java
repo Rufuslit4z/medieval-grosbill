9bbd429c774f463bb86b02b54a379713177cf2e3
Rufuslit4z <paul.marie@protonmail.com>
2018-11-17 15:14:59 +0100

    création de la base du projet

---
+++ src/main/java/com/medievalgrosbill/MedievalgrosbillApplication.java
@@ -0,0 +1,12 @@
+package com.medievalgrosbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalgrosbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalgrosbillApplication.class, args);
+	}
+}
