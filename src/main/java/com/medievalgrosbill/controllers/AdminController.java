1a3a2b3da9e48c844795f6e6ca23e2546aa7abbb
nicolasGrihault <nicolas.grihault@imie.fr>
2018-12-11 17:28:26 +0100

    jpgCarte+Xlsrapport

--- src/main/java/com/medievalgrosbill/controllers/AdminController.java
+++ src/main/java/com/medievalgrosbill/controllers/AdminController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @org.springframework.stereotype.Controller
 @RequestMapping(AdminController.BASE_URL)
 public class AdminController {
+public static final String BASE_URL = "/admins";
+private static final String BASE_PAGE_NAME = "Admins";
+//Ajouter une fonction de list des paths pour les different sous controller d'admin
 	
-	public static final String BASE_URL = "/admins";
-	private static final String BASE_PAGE_NAME = "Admins";
-	//Ajouter une fonction de list des paths pour les different sous controller d'admin
-	
-	@RequestMapping(value = {"","/","/index"}, method = RequestMethod.GET)
-	public String admins(Model model) {
-		model.addAttribute("detailPath", BASE_URL);
-		return BASE_URL+"/index";
+@RequestMapping(value = {"","/","/index"}, method = RequestMethod.GET)
+public String admins(Model model) {
+	model.addAttribute("detailPath", BASE_URL);
+	return BASE_URL+"/index";
 	}
 }
